f578f4c83f9825dfa385cd73390beef150a3085b
Denis <dis-media@rambler.ru>
2020-03-31 01:26:08 +0300

    add: header and section school + mobile menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var swiperSchool = new Swiper('.school-form__swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.school-form__next',
+      prevEl: '.school-form__prev',
+    },
+  });
+});
